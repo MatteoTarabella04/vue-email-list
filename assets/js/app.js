5c0ec512cd9465dc8c95a37de4e5d0afec906cc3
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-11 17:23:24 +0200

    add function to generate random emails

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,27 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+const { createApp } = Vue
+
+createApp({
+   data() {
+      return {
+         url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+         mails: [],
+      }
+   },
+   methods: {
+   },
+   mounted() {
+
+      for (let i = 0; i < 10; i++) {
+         axios
+         .get(this.url)
+         .then(response => {
+            console.log(response);
+            this.mails.push(response.data.response)
+         });
+      }
+   }
+}).mount('#app');
